96fefb9a2e891dd40cb2e61f05b8928d52faefbd
Jack Hubert <jphubert93@gmail.com>
2021-03-10 16:50:22 -0700

    added image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,31 @@ Macrame.create([
     {
         name: "heart coasters",
         style: "coaster",
-        image: "./assets/img/heart_coasters.png"
+        image: "/assets/img/heart-coasters.png"
     },
     {
         name: "heart keychains",
         style: "keychains",
-        image: "./assets/img/keychains.png"
+        image: "/assets/img/keychains-1.png"
     },
     {
         name: "wall hanging",
         style: "wall hanging",
-        image: "./assets/img/wall-hanging.jpg"
+        image: "assets/img/wall-hanging.jpg"
     },
     {
         name: "postinos broadway artist",
         style: "artist",
-        image: "./assets/img/the-artist.jpg"
+        image: "/assets/img/the-artist.jpg"
+    },
+    {
+        name: "jpk embellishments logo",
+        style: "logo",
+        image: "assets/img/jpk-embellishments-logo.png"
+    },
+    {
+        name: "jpk embellishments card",
+        style: "logo",
+        image: "/assets/img/jpk-embellishments-card.jpeg"
     },
 ])
