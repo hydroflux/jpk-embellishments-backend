033c9c26f9b11068d8a6f959281686b96a79d594
Jack Hubert <jphubert93@gmail.com>
2021-03-10 16:22:50 -0700

    created macrames resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,21 @@ Macrame.create([
     {
         name: "heart coasters",
         style: "coaster",
-        image: "./assets/heart_coasters.png"
+        image: "./assets/img/heart_coasters.png"
     },
     {
         name: "heart keychains",
         style: "keychains",
-        image: "./assets/keychains.png"
+        image: "./assets/img/keychains.png"
     },
     {
         name: "wall hanging",
         style: "wall hanging",
-        image: "./assets/wall-hanging.jpg"
+        image: "./assets/img/wall-hanging.jpg"
     },
     {
         name: "postinos broadway artist",
         style: "artist",
-        image: "./assets/the-artist.jpg"
+        image: "./assets/img/the-artist.jpg"
     },
 ])
